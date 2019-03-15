0960ac34dacb58dd22212fc804aa28e40b6c3c97
Gunnar Kvilhaug <gunnar.kvilhaug@appex.no>
2019-03-15 16:04:37 +0100

    Added some scripts

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
 
-// -------------------- Variables --------------------------------------------------------
+//------- Variabler ---------------------------------------------------------------------------- 
+//----------------------------------------------------------------------------------------------  
 
-const phrases = [
-    'gunnar',
-    'harald',
-    'hein tore',
-    'anders',
-    'daniel',
-    'appex',
-    'deco'
-];
-
-const overlayDiv = document.querySelector('#overlay');
-const phraseSection = document.querySelector('#phrase ul');
 const qwerty = document.querySelector('#qwerty');
-const scoreBoardImg = document.querySelectorAll('#scoreboard img');
+const phraseSection = document.querySelector('#phrase ul');
+
+const overlay = document.querySelector('#overlay');
+const scoreBoard = document.querySelectorAll('#scoreboard img');
+
 let missedScore = 0;
 
+//------- SVAR -------------------------------------------------------------------------------- 
+//--------------------------------------------------------------------------------------------- 
 
-// Press "Start Game" button to remove overlay <div>
-overlayDiv.addEventListener('click', (e) => {
+const phrases = [
+    'manchester united',
+    'ajax',
+    'manchester city',
+    'liverpoop',
+    'porto',
+    'barcelona',
+    'juventus',
+    'spurs'
+];
+
+//------- START GAME -------------------------------------------------------------------------- 
+//--------------------------------------------------------------------------------------------- 
+
+overlay.addEventListener('click', function(e) {
     if (e.target.tagName === 'A') {
-        overlayDiv.style.display = 'none';
+        overlay.style.display = 'none';
     }
 });
 
-// list of possible phrase for game
+//------- VELGE HVILKET RANDOM SVAR ----------------------------------------------------------- 
+//--------------------------------------------------------------------------------------------- 
 
-//     Return a random phrase in array
-const getRandomPhraseAsArray = (phrase) => {
+const getRandomPhrase = (phrase) => {
     let randomNum = Math.floor(Math.random() * (phrase.length));
     let randomWord = phrase[randomNum];
     let phraseArr = [];
@@ -37,89 +45,16 @@ const getRandomPhraseAsArray = (phrase) => {
     }
     return phraseArr;
 };
-
-const addPhrasetoDisplay = (array) => {
-    for (arr of array) {
+const addPhrase = function(arr) {
+    for (array of arr) {
         let li = document.createElement('li');
-        li.textContent = arr;
-        li.className = (arr === " ") ?  'space' : 'letter';
-        // if (arr == " ") {
-        //     li.className = 'space';
-        // } else {
-        //     li.className = 'letter';
-        // }
+        li.textContent = array;
+        li.className = (array === " ") ? 'space' : 'letter';
         phraseSection.appendChild(li);
     }
 }
 
-let phraseArray = getRandomPhraseAsArray(phrases);
-addPhrasetoDisplay(phraseArray);
+let phraseArr = getRandomPhrase(phrases);
+addPhrase(phraseArr);
 
-//================================================================================
-//  function to check if letter guessing is right, return the letter if true
 
-let phraseList = phraseSection.querySelectorAll('li');
-
-const checkLetter = (letter) => {
-    let result = null;
-    for (li of phraseList) {
-        if (li.className === 'letter' && li.textContent === letter) {
-            li.className = 'show';
-            result = letter;
-        }
-    }
-    return result
-};
-
-// function to run if letter guess is right, will resetGame() if no more .letter class in phrase
-const checkWin = () => {
-    let isLetter = false;
-    for (li of phraseList) {
-        li.className === 'letter' ? isLetter = true : null;
-    }
-    if (!isLetter) {
-        resetGame('win')
-    }
-};
-// function to run if letter guess is wrong, will change socreBoard img, and if missedScore accumulate to length of scoreBoardImg, will resetGame()
-const checkLose = () => {
-    scoreBoardImg[missedScore].src = 'images/lostHeart.png';
-    missedScore += 1;
-    if (missedScore === scoreBoardImg.length) {
-        resetGame('lose');
-    }
-}
-
-//  event listener to on screen keyboard picks, add a class and disabled click
-qwerty.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
-        e.target.className = 'chosen';
-        e.target.disabled = true;
-        let letterFound = checkLetter(e.target.textContent);
-        letterFound ? checkWin() : checkLose();
-    }
-});
-// function to reset the game
-const resetGame = (winLose) => {
-    setTimeout(() => {
-        overlayDiv.className = winLose;
-        overlayDiv.style.display = '';
-        overlayDiv.lastElementChild.textContent = 'Reset Game';
-        phraseSection.innerHTML = '';
-        // while (phraseSection.childElementCount > 0) {
-        //     phraseSection.removeChild(phraseSection.children[0])
-        // }
-        phraseArray = getRandomPhraseAsArray(phrases);
-        addPhrasetoDisplay(phraseArray);
-        phraseList = phraseSection.querySelectorAll('li');
-        missedScore = 0;
-        let buttons = qwerty.querySelectorAll('button');
-        buttons.forEach(button => {
-            button.className = '';
-            button.disabled = false;
-        });
-        scoreBoardImg.forEach(img => {
-            img.src = 'images/liveHeart.png'
-        })
-    }, 1000)
-};
